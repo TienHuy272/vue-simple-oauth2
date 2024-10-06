9745821de3f5e915105ca1177a65ac96fdf3107f
HuyNT <tienhuylionel@gmail.com>
2024-10-06 19:55:47 +0700

    coding simple oauth2 vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,95 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="login-container">
+    <h1>Login Page</h1>
+    <button  @click="loginWithGoogle">Login with Google</button>
+    <button style="margin-top: 30px" @click="fetchData">Fetch Data</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      authenUrl: null,
+      isLogged: false
+    }
+  },
+  methods: {
+    async loginWithGoogle() {
+      if (!this.authenUrl) {
+        await this.initAuthenUrl();
+      }
+      window.location.href = this.authenUrl; // Redirect to Google OAuth
+    },
+    async initAuthenUrl() {
+      try {
+        localStorage.removeItem("access_token");
+        const {data} = await axios.get('http://localhost:8081/auth/url'); 
+        this.authenUrl = data.url;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async initAccessToken(code) {
+      try {
+        const {data} = await axios.get(`http://localhost:8081/auth/callback?code=${code}`); 
+        localStorage.setItem("access_token", (data.token))
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async fetchData() {
+      try {
+        const {data} = await axios.get(`http://localhost:8081/private/messages`, {
+          headers: {
+            "Authorization": "Bearer " + localStorage.getItem("access_token")
+          }
+        }); 
+        alert(data.message)
+      } catch (error) {
+        alert("UNAUTHORIZED");
+        localStorage.removeItem("access_token");
+      }
+    }
+  },
+  created() {
+    let accessToken = localStorage.getItem("access_token");
+    if (accessToken) {
+      return;
+    }
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    if (code) {
+      this.initAccessToken(code);
+    } else {
+      this.initAuthenUrl();
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+button {
+  padding: 10px 20px;
+  background-color: #4285f4;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+button:hover {
+  background-color: #357ae8;
 }
 </style>
